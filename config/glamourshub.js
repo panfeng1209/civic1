43f4ac1f6d99f12a689d19e2522abd109f381e79
Your Name <you@example.com>
2021-04-18 22:32:56 +0800

    update

--- config/glamourshub.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    datas: [
-        'http://56ssc.cc/page/2/',
-        'http://56ssc.cc/page/3/',
-        'http://56ssc.cc/page/4/',
-    ],
-    selector: '.post-title'
-}
